6b6844d75ae916dc7af7bdd8c9741e922b270e82
pixeldev <amiranda8695@gmail.com>
2021-12-18 15:29:05 -0600

    build: use gradle instead of maven

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenLocal()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.10.10")
+}
+
+tasks {
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(16))
+        }
+    }
+
+    processResources {
+        filesMatching("**/*.yml") {
+            filter<org.apache.tools.ant.filters.ReplaceTokens>(
+                "tokens" to mapOf("version" to project.version)
+            )
+        }
+    }
+}
