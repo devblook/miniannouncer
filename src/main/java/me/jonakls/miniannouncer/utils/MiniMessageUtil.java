d7057bbecbf8ecb2c3d8709e37b7d3e61d60fe86
pixeldev <amiranda8695@gmail.com>
2021-12-18 20:01:50 -0600

    refactor: make it a utility

--- src/main/java/me/jonakls/miniannouncer/utils/MiniMessageUtil.java
+++ src/main/java/me/jonakls/miniannouncer/utils/MiniMessageUtil.java
@@ -1,42 +1,38 @@
-package me.jonakls.miniannouncer.managers;
+package me.jonakls.miniannouncer.utils;
 
-import me.jonakls.miniannouncer.MiniAnnouncer;
-import me.jonakls.miniannouncer.utils.ChatUtil;
 import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.title.Title;
 import org.apache.commons.lang.StringUtils;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.intellij.lang.annotations.Subst;
 
 import java.time.Duration;
 
-public class ActionsManager {
+public class MiniMessageUtil {
 
-    // TODO FIX-THIS, config var is not being used!
-    public FileConfiguration config;
-
-    public ActionsManager(MiniAnnouncer plugin) {
-        this.config = plugin.getConfig();
+    public static Component toMiniMessage(String text) {
+        return MiniMessage.miniMessage().parse(text);
     }
 
-    public void execute(Player player, String line) {
+    public static void execute(Player player, String line) {
         String actionType = StringUtils.substringBetween(line, "[", "]").toUpperCase();
 
         switch (actionType) {
             default:
             case "MESSAGE":
-                player.sendMessage(ChatUtil.toMiniMessage(line.substring(9)));
+                player.sendMessage(toMiniMessage(line.substring(9)));
                 break;
             case "ACTIONBAR":
-                player.sendActionBar(ChatUtil.toMiniMessage(line.substring(11)));
+                player.sendActionBar(toMiniMessage(line.substring(11)));
                 break;
             case "TITLE":
                 String[] title = line.substring(7).split(";");
                 player.showTitle(
                         Title.title(
-                                ChatUtil.toMiniMessage(title[0]),
-                                ChatUtil.toMiniMessage(title[1]),
+                                toMiniMessage(title[0]),
+                                toMiniMessage(title[1]),
                                 Title.Times.of(
                                         Duration.ofSeconds(Long.parseLong(title[2])),
                                         Duration.ofSeconds(Long.parseLong(title[3])),
