3144da545909bb0a1e2b863d021eba5b1cd1f4e1
pixeldev <amiranda8695@gmail.com>
2021-12-18 20:03:15 -0600

    feat: add announcement task

---
+++ src/main/java/me/jonakls/miniannouncer/announce/AnnouncementTask.java
@@ -0,0 +1,33 @@
+package me.jonakls.miniannouncer.announce;
+
+import me.jonakls.miniannouncer.message.MessageHandler;
+import me.jonakls.miniannouncer.stack.AnnouncementStack;
+import me.jonakls.miniannouncer.utils.MiniMessageUtil;
+import org.bukkit.Bukkit;
+
+public class AnnouncementTask implements Runnable {
+
+    private final AnnouncementStack announcementStack;
+    private final MessageHandler messageHandler;
+
+    public AnnouncementTask(AnnouncementStack announcementStack,
+                            MessageHandler messageHandler) {
+        this.announcementStack = announcementStack;
+        this.messageHandler = messageHandler;
+    }
+
+    @Override
+    public void run() {
+        if (announcementStack.hasNext()) {
+            Announcement announcement = announcementStack.next();
+            for (String line : announcement.getLines()) {
+                Bukkit.getOnlinePlayers()
+                        .forEach(player -> MiniMessageUtil.execute(
+                                player,
+                                messageHandler.applyInterceptors(player, line)
+                        ));
+            }
+        }
+    }
+
+}
