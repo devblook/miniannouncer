f907669e7cb3ee9ac41b1cec964fc7a44993fdb2
jonakls <asmot54@hotmail.com>
2023-04-07 17:40:08 -0500

    feat(files): add yaml configuration provider in velocity platform

--- velocity/src/main/java/me/jonakls/miniannouncer/velocity/module/PluginModule.java
+++ velocity/src/main/java/me/jonakls/miniannouncer/velocity/module/PluginModule.java
@@ -1,6 +1,11 @@
 package me.jonakls.miniannouncer.velocity.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import me.jonakls.miniannouncer.core.configuration.YamlPluginConfiguration;
+import me.jonakls.miniannouncer.core.configuration.sections.Configuration;
 
 import java.nio.file.Path;
 
@@ -12,8 +17,16 @@ public class PluginModule extends AbstractModule {
         this.pluginDirectory = pluginDirectory;
     }
 
+    @Singleton
+    @Provides
+    public YamlPluginConfiguration<Configuration> provideConfiguration(@DataDirectory Path pluginDirectory) {
+        return YamlPluginConfiguration.create(pluginDirectory, Configuration.class, "config");
+    }
+
     @Override
     protected void configure() {
         bind(PluginModule.class).toInstance(this);
+
+
     }
 }
