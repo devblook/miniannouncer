762e9d439d4cef47291049d8c128bcf695fe8d7c
jonakls <asmot54@hotmail.com>
2023-06-03 12:53:25 -0500

    refactor(identation): apply code style from editorconfig rules

--- src/main/java/me/jonakls/miniannouncer/module/submodules/CommandModule.java
+++ src/main/java/me/jonakls/miniannouncer/module/submodules/CommandModule.java
@@ -2,7 +2,7 @@ package me.jonakls.miniannouncer.module.submodules;
 
 import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
 import dev.triumphteam.cmd.core.BaseCommand;
-import me.jonakls.miniannouncer.MiniAnnouncer;
+import me.jonakls.miniannouncer.MiniAnnouncerPlugin;
 import me.jonakls.miniannouncer.command.MainCommand;
 import org.bukkit.command.CommandSender;
 import team.unnamed.inject.AbstractModule;
@@ -12,23 +12,23 @@ import javax.inject.Singleton;
 
 public class CommandModule extends AbstractModule {
 
-    private final MiniAnnouncer plugin;
+  private final MiniAnnouncerPlugin plugin;
 
-    public CommandModule(MiniAnnouncer plugin) {
-        this.plugin = plugin;
-    }
+  public CommandModule(MiniAnnouncerPlugin plugin) {
+    this.plugin = plugin;
+  }
 
-    @Singleton
-    @Provides
-    public BukkitCommandManager<CommandSender> commandManager() {
-        return BukkitCommandManager.create(plugin);
-    }
+  @Singleton
+  @Provides
+  public BukkitCommandManager<CommandSender> commandManager() {
+    return BukkitCommandManager.create(plugin);
+  }
 
-    @Override
-    protected void configure() {
-        multibind(BaseCommand.class)
-                .asSet()
-                .to(MainCommand.class)
-                .singleton();
-    }
+  @Override
+  protected void configure() {
+    multibind(BaseCommand.class)
+      .asSet()
+      .to(MainCommand.class)
+      .singleton();
+  }
 }
