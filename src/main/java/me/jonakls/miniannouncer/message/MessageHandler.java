d3ecc687c46bf529db4f40f6185f5ed55f1b6fdb
jonakls <asmot54@hotmail.com>
2023-06-03 12:50:59 -0500

    feat(message): remove the different interceptors to manage a single interceptor that processes placeholder api or not as the case may be.
    refactor(identation): apply code style from editorconfig rules

--- src/main/java/me/jonakls/miniannouncer/message/MessageHandler.java
+++ src/main/java/me/jonakls/miniannouncer/message/MessageHandler.java
@@ -3,91 +3,93 @@ package me.jonakls.miniannouncer.message;
 import me.jonakls.miniannouncer.BukkitConfiguration;
 import me.jonakls.miniannouncer.utils.MiniMessageUtil;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class MessageHandler {
 
-    private static final String BASE_PATH = "messages.";
+  private static final String BASE_PATH = "messages.";
+  private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
 
-    private final Set<MessageInterceptor> interceptors;
-    private final BukkitConfiguration config;
+  private final BukkitConfiguration config;
 
-    public MessageHandler(BukkitConfiguration config) {
-        this.config = config;
-        this.interceptors = new HashSet<>();
+  private MessageHandler(BukkitConfiguration config) {
+    this.config = config;
+  }
+
+  public static MessageHandler create(BukkitConfiguration config) {
+    return new MessageHandler(config);
+  }
+
+  public Component intercept(CommandSender sender, String message) {
+    final var pluginManager = Bukkit.getServer()
+                                .getPluginManager();
+
+    if (pluginManager.getPlugin("PlaceholderAPI") != null) {
+      return MINI_MESSAGE.deserialize(message, MiniMessageUtil.papiResolver((Player) sender));
     }
 
-    public Component applyInterceptors(CommandSender sender, String message) {
-        for (MessageInterceptor interceptor : interceptors) {
-            message = interceptor.intercept(sender, message);
-        }
+    return MINI_MESSAGE.deserialize(message);
+  }
 
-        return MiniMessageUtil.toMiniMessage(message);
+  public Component getMessage(
+    CommandSender sender, String path,
+    Object... replacements
+  ) {
+    FileConfiguration configuration = config.get();
+    String message = configuration.getString(BASE_PATH + path);
+
+    if (message == null) {
+      return Component.text(path);
     }
 
-    public Component applyInterceptors(CommandSender sender, Collection<String> strings) {
-        Component message = Component.empty();
-        for (String string : strings) {
-            message = message.append(applyInterceptors(sender, string));
-        }
-        return message;
+    return intercept(
+      sender,
+      String.format(message, replacements)
+    );
+  }
+
+  public List<Component> getMessages(
+    CommandSender sender, String path,
+    Object... replacements
+  ) {
+    FileConfiguration configuration = config.get();
+    List<String> messages = configuration.getStringList(BASE_PATH + path);
+    List<Component> components = new ArrayList<>(List.of(Component.empty()));
+
+    if (messages.isEmpty()) {
+      return Collections.singletonList(Component.text(path));
     }
 
-    public Component getMessage(CommandSender sender, String path,
-                             Object... replacements) {
-        FileConfiguration configuration = config.get();
-        String message = configuration.getString(BASE_PATH + path);
-
-        if (message == null) {
-            return Component.text(path);
-        }
-
-        return applyInterceptors(
-                sender,
-                String.format(message, replacements)
-        );
+    for (String message : messages) {
+      components.add(intercept(
+        sender,
+        String.format(message, replacements)
+      ));
     }
 
-    public List<Component> getMessages(CommandSender sender, String path,
-                                    Object... replacements) {
-        FileConfiguration configuration = config.get();
-        List<String> messages = configuration.getStringList(BASE_PATH + path);
-        List<Component> components = new ArrayList<>(List.of(Component.empty()));
+    return components;
+  }
 
-        if (messages.isEmpty()) {
-            return Collections.singletonList(Component.text(path));
-        }
+  public void sendMessage(
+    CommandSender sender, String path,
+    Object... replacements
+  ) {
+    sender.sendMessage(getMessage(sender, path, replacements));
+  }
 
-        for (String message : messages) {
-            components.add(applyInterceptors(
-                    sender,
-                    String.format(message, replacements)
-            ));
-        }
-
-        return components;
-    }
-
-    public void sendMessage(CommandSender sender, String path,
-                            Object... replacements) {
-        sender.sendMessage(getMessage(sender, path, replacements));
-    }
-
-    public void sendListMessage(CommandSender sender, String path,
-                             Object... replacements) {
-        getMessages(sender, path, replacements)
-                .forEach(sender::sendMessage);
-    }
-
-    public void addInterceptor(MessageInterceptor interceptor) {
-        interceptors.add(interceptor);
-    }
+  public void sendListMessage(
+    CommandSender sender, String path,
+    Object... replacements
+  ) {
+    getMessages(sender, path, replacements)
+      .forEach(sender::sendMessage);
+  }
 }
